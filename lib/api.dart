934aa7b610b5096ac7cb11daffae93392c75d800
unknown <karkiaku000@gmail.com>
2026-01-22 09:49:59 +0545

    fantasy api connection

---
+++ lib/api.dart
@@ -0,0 +1,9 @@
+class ApiConstants {
+  ApiConstants._();
+
+  static const String baseUrl = "https://fantasytest.livescorenepal.com/api";
+  static const String loginEndPoint = "$baseUrl/login";
+  static const String registerEndPoint = "$baseUrl/login";
+  static const String profileEndPoint = "$baseUrl/login";
+  static const String logoutEndPoint = "$baseUrl/login";
+}
